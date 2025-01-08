9b0dec39d07b0e8734d01a6f3822476d5156998d
Andres Sorto <andresismaelsortozuniga@gmail.com>
2025-01-08 13:42:57 -0600

    Manejo de rutas agregado

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { AppRouter } from './router/AppRouter'
 
 export const CalendarApp = () => {
   return (
-    <div>CalendarApp</div>
+    <BrowserRouter>
+      <AppRouter/>
+    </BrowserRouter>
   )
 }
