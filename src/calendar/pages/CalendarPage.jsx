8f1b9f81ae0b1b8d715fab0c4bf19301a0afe5df
Andres Sorto <andresismaelsortozuniga@gmail.com>
2025-01-09 11:22:37 -0600

    react-big-calendar agregado al proyecto

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+import { addHours, format, getDay, parse, startOfWeek } from 'date-fns'
+import { enUS } from 'date-fns/locale/en-US'
+
 import { NavBar } from '../'
+
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [{
+  title: 'Cumpleaños del Jefe',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours( new Date(), 2 ),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Andres'
+  }
+}]
 
 export const CalendarPage = () => {
   return (
     <>
       <NavBar/>
+
+      <Calendar
+        localizer={ localizer }
+        events={ events }
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px )' }}
+      />
+
     </>
   )
 }
